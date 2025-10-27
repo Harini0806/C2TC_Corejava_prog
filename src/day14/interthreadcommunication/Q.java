73a13111ec5ce3b20267cef320e0a63af6ad3cc5
Harini <harinin0806@gmail.com>
2025-10-27 14:35:03 +0530

    commit

---
+++ src/day14/interthreadcommunication/Q.java
@@ -0,0 +1,40 @@
+package day14.interthreadcommunication;
+
+public class Q {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		ublic class Q {
+			int n;
+			boolean valueSet = false;
+
+			public synchronized void put(int n) {
+				if (valueSet) {
+					try {
+						wait();
+					} catch (Exception e) {
+						System.out.println(e);
+					}
+				}
+				this.n = n;
+				valueSet = true;
+				System.out.println("Put : " + n);
+				notify();
+			}
+
+			public synchronized void get() {
+				if (!valueSet) {
+					try {
+						wait();
+					} catch (Exception e) {
+						System.out.println(e);
+					}
+				}
+				System.out.println("Getting data " + n);
+				valueSet = false;
+				notify();
+			}
+		}
+	}
+
+}

abde6f93c4a039a03e3a7d023ce10e436b761446
Harini <harinin0806@gmail.com>
2025-10-27 14:31:20 +0530

    commit

---
+++ src/day14/interthreadcommunication/Consumer.java
@@ -0,0 +1,28 @@
+package day14.interthreadcommunication;
+
+public class Consumer {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		public class Consumer extends Thread {
+			Q obj; 
+
+			public Consumer(Q obj) {
+				this.obj = obj;
+				start();
+			}
+
+			public void run() {
+					while (true) {
+					try {
+						Thread.sleep(3000);
+					} catch (Exception e) {
+						System.out.println(e);
+					}
+					obj.get();
+				}
+			}
+		}
+	}
+
+}

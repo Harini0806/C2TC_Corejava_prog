38a3e097ac0c17922b74f5f47460de3a2f988d78
Harini <harinin0806@gmail.com>
2025-10-27 14:39:05 +0530

    commit

---
+++ src/day14/synchronization/bank.java
@@ -0,0 +1,15 @@
+package day14.synchronization;
+
+public class bank {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		public interface Bank {
+			int MINBAL=5000; //fields are static and final
+			static final int DAILY_LIMIT=25000;
+			public abstract void deposit(int amt) throws DepositLimitExceedsException;
+			void withdraw(int amt)throws InsufficientBalanceException; //public and abstract
+		}
+	}
+
+}
